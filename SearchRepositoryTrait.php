f5f06a35c46c7a5f6714d1ace8dc4c84c66e546f
michael <m@cadot.eu>
2023-04-30 21:57:55 +0200

    refactor(SearchRepositoryTrait.php): réorganiser la fonction search pour prendre en compte les champs de recherche et les champs de tri séparément. Ajouter un paramètre pour filtrer par état.

--- SearchRepositoryTrait.php
+++ SearchRepositoryTrait.php
@@ -4,47 +4,25 @@ namespace App\Repository\base;
 
 trait SearchRepositoryTrait
 {
-          /** Function php pour lancer une recherche dans le repository
-         * example: $...Repository->search(['couleur' => 'rouge', 'marque' => '!renault'],deletd:true)
-         * @param array searchs tableau avec valeur de recherche et! pour not like
-         * @param string sort champ de tri par défaut id
-         * @param string direction ASC ou DESC
-         * @param string categorie nom de la categorie
-         * @param bool deleted true pour afficher les supprimés
-         * @param array etats tableau avec les états ['en cours','terminé']
-         *
-         */
-    public function search(array $searchs = [], string $sort = 'id', ?string $direction = 'ASC', ?string $categorie = null, ?bool $deleted = false, array $etats = []): ?array
+    public function index($search, $fields, $sort, $direction, $categorie = null, $deleted = false, $etat = null)
     {
+        $sort = is_null($sort) ? 'a.id' : $sort;
         $qb = $this->createQueryBuilder('a');
         if ($deleted) {
-                $qb->where($qb->expr()->isNotNull('a.deletedAt'));
+            $qb->where($qb->expr()->isNotNull('a.deletedAt'));
         } else {
             $qb->where($qb->expr()->isNull('a.deletedAt'));
         }
+        if ($etat != null) {
+            $qb->andwhere($qb->expr()->eq('a.etat', ':etat'))
+              ->setParameter('etat', $etat);
+        }
         $ORX = $qb->expr()->orx();
-        //les recherches
-        foreach ($searchs as $field => $search) {
+        foreach ($fields as $field) {
             $ors = [];
             foreach (explode(' ', $search) as $s) {
                 $s = str_replace("'", "''", $s);
-                if ($s[0] == '!') {
-                    $ors[] = $qb->expr()->orx("a.$field NOT LIKE '%" . substr($s, 1) . "%' ");
-                } else {
-                    $ors[] = $qb->expr()->orx("a.$field LIKE '%$s%' ");
-                }
-            }
-            $ORX->add(join(' AND ', $ors));
-        }
-                $qb->andWhere($ORX);
-        //les états
-            $ors = [];
-        foreach ($etats as $etat) {
-                $s = str_replace("'", "''", $etat);
-            if ($s[0] == '!') {
-                $ors[] = $qb->expr()->orx("a.etat NOT LIKE '%" . substr($s, 1) . "%' ");
-            } else {
-                $ors[] = $qb->expr()->orx("a.etat LIKE '%$s%' ");
+                $ors[] = $qb->expr()->orx("a.$field LIKE '%$s%' ");
             }
             $ORX->add(join(' AND ', $ors));
         }
@@ -52,6 +30,51 @@ trait SearchRepositoryTrait
         if ($categorie != null) {
             $qb->andwhere($qb->expr()->isMemberOf(':categorie', 'a.categories'))->setParameter('categorie', $categorie);
         }
-                return $qb->orderBy('a.' . $sort, strtoupper($direction))->getQuery()->getResult();
+        return $qb->orderBy($sort, strtoupper($direction))
+        ->getQuery()
+        ->getResult();
+    }
+
+    /**
+     * Function for searchs
+     * exemple: ->search(['roles' => '!SUPERADMIN'], deleted:false)
+     *
+     * @param array searchs An array of fields ex:
+     * @param sort The field to sort the results by. It defaults to 'id' if not provided.
+     * @param direction ASC or DESC
+     * @param categorie The "categorie" parameter is a string that represents a category
+     * @param deleted  If set to false or null, only non-deleted records will be returned.
+     * @return ?array an array of results
+     */
+    public function search(array $searchs = [], ?string $sort = 'id', ?string $direction = 'ASC', ?string $categorie = null, ?bool $deleted = false): ?array
+    {
+        $qb = $this->createQueryBuilder('a');
+        if ($deleted) {
+            $qb->where($qb->expr()->isNotNull('a.deletedAt'));
+        } else {
+            $qb->where($qb->expr()->isNull('a.deletedAt'));
+        }
+        $ORX = $qb->expr()->orx();
+        //les recherches
+         $ors = [];
+           //si le field est composé de field séparé par des virgules
+        if (strlen($searchs[key($searchs)])) {
+            $search = $searchs[key($searchs)];
+            foreach (explode(',', key($searchs)) as $field) {
+                $s = str_replace("'", "''", $search);
+                if ($s[0] == '!') {
+                    $ors[] = $qb->expr()->orx("a.$field NOT LIKE '%" . substr($s, 1) . "%' ");
+                } else {
+                    $ors[] = $qb->expr()->orx("a.$field LIKE '%$s%' ");
+                }
+                $ORX->add(join(' AND ', $ors));
+            }
+        }
+        $qb->andWhere($ORX);
+        if ($categorie != null) {
+            $qb->andwhere($qb->expr()->isMemberOf(':categorie', 'a.categories'))->setParameter('categorie', $categorie);
+        }
+
+            return $qb->orderBy('a.' . $sort, strtoupper($direction))->getQuery()->getResult();
     }
 }
